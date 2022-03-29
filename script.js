ff38411ed2c005115301aa4d5bf136126736dc5f
Sergio <sergiofiadanana@gmail.com>
2022-03-29 08:04:47 +0000

    My project commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    if(principal <= 0){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }else{
+    document.getElementById("result").innerHTML="If you deposit \<span class\=\"loko\"\>"+principal+",\<\/span\>\<br\>at an interest rate of \<span class\=\"loko\"\>"+rate+"%\<\/span\>\<br\>You will receive an amount of \<span class\=\"loko\"\>"+interest+",\<\/span\>\<br\>in the year \<span class\=\"loko\"\>"+year+"\<\/span\>\<br\>"
 }
-        
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}     
